82fd5bf84b3349b7c15cc7c814f71598f7cecf55
trautslol <trauts@hotmail.fr>
2016-10-12 13:35:15 +0200

    CAPITALE

--- ar2rom.py
+++ ar2rom.py
@@ -3,37 +3,37 @@ import unittest
 
 def ar2rom(val):
     if val == 1:
-        return 'i'
+        return 'I'
     elif val == 2:
-        return 'ii'
+        return 'II'
     elif val == 3:
-        return 'iii'
+        return 'III'
 
 
 class Tests(unittest.TestCase):
     def test_1_to_i(self):
-        self.assertEqual(ar2rom(1), 'i')
+        self.assertEqual(ar2rom(1), 'I')
 
     def test_2_to_ii(self):
-        self.assertEqual(ar2rom(2), 'ii')
+        self.assertEqual(ar2rom(2), 'II')
 
     def test_3_to_iii(self):
-        self.assertEqual(ar2rom(3), 'iii')
+        self.assertEqual(ar2rom(3), 'III')
 
     def test_4_to_iv(self):
-        self.assertEqual(ar2rom(4), 'iv')
+        self.assertEqual(ar2rom(4), 'IV')
 
     def test_5_to_v(self):
-        self.assertEqual(ar2rom(5), 'v')
+        self.assertEqual(ar2rom(5), 'V')
 
     def test_10_to_x(self):
-        self.assertEqual(ar2rom(10), 'x')
+        self.assertEqual(ar2rom(10), 'X')
 
     def test_20_to_xx(self):
-        self.assertEqual(ar2rom(20), 'xx')
+        self.assertEqual(ar2rom(20), 'XX')
 
     def test_21_to_xxi(self):
-        self.assertEqual(ar2rom(21), 'xxi')
+        self.assertEqual(ar2rom(21), 'XXI')
 
 if __name__ == "__main__":
     unittest.main()
